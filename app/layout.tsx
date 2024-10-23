dad812d702102afb93a22bc6f2c2dafc6a32fc7f
Adrian <contact@jsmastery.pro>
2024-10-23 10:36:14 +0200

    ESLint & Prettier Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import React from "react";
+
 import "./globals.css";
 
 const geistSans = localFont({
@@ -7,6 +9,7 @@ const geistSans = localFont({
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 const Home = () => (
-  <h1 className="text-3xl text-white font-black">Welcome to the world of Next.js</h1>
+  <h1 className="text-3xl font-black text-white">
+    Welcome to the world of Next.js
+  </h1>
 );
 
 export default Home;
