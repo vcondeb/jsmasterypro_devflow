4af15966ae046fae2645613d8efb8313c5a530e6
Adrian <contact@jsmastery.pro>
2024-10-23 15:37:57 +0200

    Setup Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
-const Home = () => <h1 className="h1-bold">Welcome to the world of Next.js</h1>;
+const Home = () => (
+  <>
+    <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+  </>
+);
 
 export default Home;
