50a229146561efa63941ee85650f6a223b1e97fe
Adrian <contact@jsmastery.pro>
2024-11-21 18:38:07 +0100

    Implement a Fetch Handler

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,8 +5,8 @@ import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import { api } from "@/lib/api";
 import handleError from "@/lib/handlers/error";
-import { ValidationError } from "@/lib/http-errors";
 
 const questions = [
   {
@@ -51,10 +51,7 @@ const questions = [
 
 const test = async () => {
   try {
-    throw new ValidationError({
-      title: ["Required"],
-      tags: ['"JavaScript" is not a valid tag.'],
-    });
+    return await api.users.getAll();
   } catch (error) {
     return handleError(error);
   }
@@ -65,7 +62,9 @@ interface SearchParams {
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-  await test();
+  const users = await test();
+
+  console.log(users);
 
   const { query = "", filter = "" } = await searchParams;
 
